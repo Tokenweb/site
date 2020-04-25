94b910a0c085b5e17a6712e75e01a0266eac4e2f
cryptnotiq <43958441+cryptnotiq@users.noreply.github.com>
2020-04-25 15:56:48 +0200

    updated investment funds section

--- src/pages/Home/indexMain.js
+++ src/pages/Home/indexMain.js
@@ -60,13 +60,7 @@ class IndexMain extends Component {
 
                 {/* Pricing */}
                 <PricingAndPayment />
-                <div className="position-relative">
-                    <div className="shape overflow-hidden text-light">
-                        <svg viewBox="0 0 2880 250" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M720 125L2160 0H2880V250H0V125H720Z" fill="currentColor"></path>
-                        </svg>
-                    </div>
-                </div>
+
                 {/* Faq */}
                 <Faq />
 
--- src/pages/Home/pricing-and-payment.js
+++ src/pages/Home/pricing-and-payment.js
@@ -35,18 +35,17 @@ class PricingAndPayment extends Component {
                                 <div className="p-4">
                                     <div className="d-flex justify-content-center mb-4">
 
-                                        <span className="h6 align-self-end font-weight-bold text-uppercase mb-2 ml-2">This fund invests in proven token based projects our team has been involved with for years.
-</span>
+                                        <span className="h6 align-self-end mb-2 ml-2">Invests in token projects with established history.</span>
                                     </div>
 
                                     <ul className="feature list-inline">
-                                        <li className="h5 font-weight-normal"><i className="mdi mdi-check-decagram text-primary mr-2"></i>Minimum Investment: 500 CHF</li>
-                                        <li className="h5 font-weight-normal"><i className="mdi mdi-check-decagram text-primary mr-2"></i>CHF 500 Mininum Investment</li>
-                                        <li className="h5 font-weight-normal"><i className="mdi mdi-check-decagram text-primary mr-2"></i>Control payout timing</li>
-                                        <li className="h5 font-weight-normal"><i className="mdi mdi-check-decagram text-primary mr-2"></i>24×7 support</li>
+
+                                        <li className="h5 font-weight-normal"><i className="mdi mdi-check-decagram text-primary mr-2"></i>CHF 2,500 Minimum</li>
+                                        <li className="h5 font-weight-normal"><i className="mdi mdi-check-decagram text-primary mr-2"></i>5 Year Holding Period</li>
+                                        <li className="h5 font-weight-normal"><i className="mdi mdi-check-decagram text-primary mr-2"></i>100% Capital in Tokens</li>
                                     </ul>
 
-                                    <Link to="" className="btn btn-primary btn-block mt-4 pt-2">Payment Now</Link>
+
                                 </div>
                             </div>
                         </Col>
@@ -59,16 +58,16 @@ class PricingAndPayment extends Component {
 
                                 <div className="p-4">
                                     <div className="d-flex justify-content-center mb-4">
-                                        <span className="price text-primary font-weight-bold display-4 mb-0">2</span>
-                                        <span className="h4 mb-0 mt-2 text-primary">%</span>
-                                        <span className="h6 align-self-end font-weight-bold text-uppercase mb-2 ml-2"> Per Transection</span>
+
+
+                                      <span className="h6 align-self-end mb-2 ml-2">Invests in stakable tokens and pays out staking revenue</span>
                                     </div>
 
                                     <ul className="feature list-inline">
-                                        <li className="h5 font-weight-normal"><i className="mdi mdi-check-decagram text-primary mr-2"></i>Transparent payouts</li>
-                                        <li className="h5 font-weight-normal"><i className="mdi mdi-check-decagram text-primary mr-2"></i>Deposit tagging</li>
-                                        <li className="h5 font-weight-normal"><i className="mdi mdi-check-decagram text-primary mr-2"></i>Control payout timing</li>
-                                        <li className="h5 font-weight-normal"><i className="mdi mdi-check-decagram text-primary mr-2"></i>24×7 support</li>
+                                        <li className="h5 font-weight-normal"><i className="mdi mdi-check-decagram text-primary mr-2"></i>CHF 500 Minimum</li>
+                                        <li className="h5 font-weight-normal"><i className="mdi mdi-check-decagram text-primary mr-2"></i>Withdraw Anytime</li>
+
+                                        <li className="h5 font-weight-normal"><i className="mdi mdi-check-decagram text-primary mr-2"></i>Monthly Payouts</li>
                                     </ul>
 
                                     <Link to="" className="btn btn-primary btn-block mt-4 pt-2">Payment Now</Link>
@@ -79,21 +78,20 @@ class PricingAndPayment extends Component {
                         <Col lg="4" md="6" xs="12" className="mt-4 pt-2">
                             <div className="rounded border bg-light shadow">
                                 <div className="bg-primary p-4 text-center rounded-top">
-                                    <h4 className="mb-0 title-dark text-light">Liquidity Fund I</h4>
+                                    <h4 className="mb-0 title-dark text-light">Ecosystem Fund I</h4>
                                 </div>
 
                                 <div className="p-4">
                                     <div className="d-flex justify-content-center mb-4">
-                                        <span className="price text-primary font-weight-bold display-4 mb-0">2</span>
-                                        <span className="h4 mb-0 mt-2 text-primary">%</span>
-                                        <span className="h6 align-self-end font-weight-bold text-uppercase mb-2 ml-2"> Per Transection</span>
+
+                                        <span className="h6 align-self-end mb-2 ml-2">Invests in projects which support token growth.</span>
                                     </div>
 
                                     <ul className="feature list-inline">
-                                        <li className="h5 font-weight-normal"><i className="mdi mdi-check-decagram text-primary mr-2"></i>Transparent payouts</li>
-                                        <li className="h5 font-weight-normal"><i className="mdi mdi-check-decagram text-primary mr-2"></i>Deposit tagging</li>
-                                        <li className="h5 font-weight-normal"><i className="mdi mdi-check-decagram text-primary mr-2"></i>Control payout timing</li>
-                                        <li className="h5 font-weight-normal"><i className="mdi mdi-check-decagram text-primary mr-2"></i>24×7 support</li>
+                                        <li className="h5 font-weight-normal"><i className="mdi mdi-check-decagram text-primary mr-2"></i>CHF 5,000 Minimum</li>
+                                        <li className="h5 font-weight-normal"><i className="mdi mdi-check-decagram text-primary mr-2"></i>Equity, No Tokens</li>
+                                        <li className="h5 font-weight-normal"><i className="mdi mdi-check-decagram text-primary mr-2"></i>Close Ended</li>
+
                                     </ul>
 
                                     <Link to="" className="btn btn-primary btn-block mt-4 pt-2">Payment Now</Link>
